96e5c700f7b1778a986d0721c23686aa1bcacc5f
Marco <marco1@live.de>
2014-11-20 23:06:14 +0100

    Added server features for Firebase replacement

--- Server/android-ddp-meteor.js
+++ Server/android-ddp-meteor.js
@@ -24,6 +24,83 @@ if (Meteor.isServer) {
 		};
 	}
 
+	// namespace for features that are specific to Firebase
+	var Firebase = {};
+
+	Firebase.upsert = function (query, update) {
+		// process any placeholders that may be included in this update
+		update = Firebase.processPlaceholders(update);
+		// separate this update itself from possible child updates that may be included in the value
+		update = Firebase.extractChildUpdates(update);
+
+		// if child updates were included in the value execute those first
+		if (update.children !== null) {
+			var childId;
+			// loop over all child paths/IDs with their associated updates
+			for (var childKey in update.children) {
+				if (update.children.hasOwnProperty(childKey)) {
+					// construct the ID for the child path
+					childId = query._id + "/" + childKey;
+					// recursively execute an upsert for the current child
+					Firebase.upsert({ "_id": childId }, { "_value": update.children[childKey] });
+				}
+			}
+		}
+
+		// execute the original update itself (with children removed from its value)
+		return Database.update(query, { "$set": update.self }, { "upsert": true });
+	};
+
+	Firebase.processPlaceholders = function (update) {
+		// if this update contains value that is an object
+		if (typeof update._value === "object") {
+			// if the value is a placeholder
+			if (typeof update._value._placeholder === "string") {
+				// if the placeholder is for the current timestamp
+				if (update._value._placeholder === "timestamp") {
+					// replace the value with the timestamp
+					update._value = Date.now();
+					// and return the modified update
+					return update;
+				}
+			}
+		}
+
+		// otherwise just return the unchanged update
+		return update;
+	};
+
+	Firebase.extractChildUpdates = function (update) {
+		// if this update doesn't contain any value to be written
+		if (typeof update._value === "undefined" || update._value === null) {
+			// just return the unchanged update with no children
+			return { "self": update, "children": {} };
+		}
+
+		// if the value from this update is not an object
+		if (typeof update._value !== "object") {
+			// it cannot include any child updates so just return the unchanged update
+			return { "self": update, "children": {} };
+		}
+
+		// prepare a container holding the child updates by path/ID
+		var childUpdates = {};
+
+		// loop over all key/value pairs in the value
+		for (var childKey in update._value) {
+			if (update._value.hasOwnProperty(childKey)) {
+				// save the extracted child update with its path/ID
+				childUpdates[childKey] = update._value[childKey];
+			}
+		}
+
+		// unset the value for the current path
+		update._value = null;
+
+		// return the modified update with its single child updates
+		return { "self": update, "children": childUpdates };
+	};
+
 	// clients may subscribe to all events at once by supplying the name `auto`
 	Meteor.publish("auto", function () {
 		return Database.find({});
@@ -49,6 +126,9 @@ if (Meteor.isServer) {
 		},
 		"reset": function () {
 			return Database.remove({});
+		},
+		"Firebase.upsert": function (query, update) {
+			return Firebase.upsert(query, update);
 		}
 	});
 
